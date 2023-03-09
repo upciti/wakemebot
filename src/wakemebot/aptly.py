9b73939e2852d83d81ed4c7ae0bbde1c1d301643
Simon Guigui <simon.guigui@parkoview.com>
2023-03-09 13:03:07 +0100

    refactor: rewrite aptly client

--- src/wakemebot/aptly.py
+++ src/wakemebot/aptly.py
@@ -1,22 +1,35 @@
-import base64
-import sys
-import uuid
-from contextlib import contextmanager
+import contextlib
 from dataclasses import dataclass
-from functools import cmp_to_key, partial
-from itertools import groupby
-from operator import attrgetter
+from functools import total_ordering
 from pathlib import Path
-from tempfile import TemporaryDirectory
-from typing import Callable, Generator, Iterator, List, Set
+from typing import Generator, Iterable
+from uuid import uuid4
 
 import httpx
-from debian.debian_support import Version, version_compare
-from semver.version import Version as Semver
+from debian.debian_support import Version
+from pydantic import BaseModel
+
+
+def to_camel(string: str) -> str:
+    return "".join(word.capitalize() for word in string.split("_"))
+
+
+class AptlyClientError(Exception):
+    def __init__(self, message: str, response: httpx.Response | None = None) -> None:
+        self.response = response
+        super().__init__(message)
 
 
 @dataclass(frozen=True)
-class Package:
+class MTLSCredentials:
+    ca_cert_path: Path
+    client_cert_path: Path
+    client_key_path: Path
+
+
+@total_ordering
+@dataclass(frozen=True)
+class AptlyPackageRef:
     arch: str
     name: str
     version: str
@@ -26,192 +39,136 @@ class Package:
     def upstream_version(self) -> str:
         return Version(self.version).upstream_version or ""
 
-    def __str__(self) -> str:
+    @property
+    def ref(self) -> str:
         return f"P{self.arch} {self.name} {self.version} {self.uid}"
 
-
-def sort_cmp(p1: Package, p2: Package) -> int:
-    if (version_order := version_compare(p1.version, p2.version)) != 0:
-        return version_order
-    if p1.arch < p2.arch:
-        return -1
-    if p1.arch > p2.arch:
-        return 1
-    return 0
+    def __lt__(self, other: "AptlyPackageRef") -> bool:
+        return (self.name, Version(self.version)) < (other.name, Version(other.version))
 
 
-@contextmanager
-def client_factory(
-    server_url: str, ca_cert: str, client_cert: str, client_key: str
-) -> Generator[httpx.Client, None, None]:
-    with TemporaryDirectory(prefix="wakemebot_") as base_directory:
-        ca_cert_path = Path(base_directory) / "ca.crt"
-        ca_cert_path.write_bytes(base64.b64decode(ca_cert))
-        client_cert_path = Path(base_directory) / "client.crt"
-        client_cert_path.write_bytes(base64.b64decode(client_cert))
-        client_key_path = Path(base_directory) / "client.key"
-        client_key_path.write_bytes(base64.b64decode(client_key))
-        transport = httpx.HTTPTransport(
-            retries=3,
-            cert=(str(client_cert_path), str(client_key_path)),
-            verify=str(ca_cert_path),
-        )
-        yield httpx.Client(transport=transport, base_url=server_url, timeout=120)
+class AptlyRepo(BaseModel):
+    name: str
+    comment: str
+    default_distribution: str
+    default_component: str
+
+    class Config:
+        alias_generator = to_camel
 
 
-def parse_packages(data: List[str]) -> List[Package]:
-    data = [p[1:] for p in data if p[0] == "P"]
-    return [Package(*p.split(" ")) for p in data]
+_REPO_DOES_NOT_EXIST_ERROR = "Repo '{}' does not exist"
 
 
-def list_packages(client: httpx.Client, repo: str) -> List[Package]:
-    data = client.get(f"/repos/{repo}/packages").json()
-    return parse_packages(data)
+class AptlyClient:
+    def __init__(
+        self,
+        server_url: str,
+        credentials: MTLSCredentials | None = None,
+        retries: int = 2,
+    ):
+        if credentials:
+            transport = httpx.HTTPTransport(
+                retries=retries,
+                cert=(
+                    str(credentials.client_cert_path),
+                    str(credentials.client_key_path),
+                ),
+                verify=str(credentials.ca_cert_path),
+            )
+        else:
+            transport = httpx.HTTPTransport(retries=retries)
+        self._client = httpx.Client(transport=transport, base_url=server_url, timeout=60)
 
-
-def purge_old_versions(packages: List[Package], retain_how_many: int) -> List[Package]:
-    """
-    Only keep retain_how_many versions of a package.
-    Assumes packages have the same name and arch, and are sorted by versions.
-    """
-    should_delete = packages[:-retain_how_many]
-    return should_delete
-
-
-def purge_old_revisions(packages: List[Package]) -> List[Package]:
-    """
-    Only keep the latest package revision.
-    Assumes packages have the same name and arch, and are sorted by versions.
-    """
-    should_delete: List[Package] = []
-    for key, group in groupby(packages, attrgetter("upstream_version")):
-        package_group = sorted(group, key=cmp_to_key(sort_cmp))
-        should_delete.extend(package_group[:-1])
-    return should_delete
-
-
-def purge_old_patches(packages: List[Package]) -> List[Package]:
-    """
-    Only keep the most recent patch for packages using semantic versioning.
-    Example: if we have two releases with version 2.1.1 and 2.2.2, 2.2.1 will be removed.
-    Assumes packages have the same name and arch, and are sorted by versions.
-    """
-    should_delete: List[Package] = []
-    packages = [p for p in packages if Semver.isvalid(p.upstream_version)]
-
-    def group_by_key(package: Package) -> Semver:
-        return Semver.parse(package.upstream_version).next_version(part="minor")
-
-    for key, group in groupby(packages, key=group_by_key):
-        package_group = sorted(group, key=cmp_to_key(sort_cmp))
-        should_delete.extend(package_group[:-1])
-
-    return should_delete
-
-
-def purge_old_packages(packages: List[Package], retain_how_many: int) -> Set[Package]:
-    """
-    Call the three purge functions defined above.
-    Assumes packages have the same name and arch.
-    """
-    # packages are only sorted once here
-    packages = sorted(packages, key=cmp_to_key(sort_cmp))
-    packages_to_delete: Set[Package] = set()
-    purge_functions: List[Callable[[List[Package]], List[Package]]] = [
-        purge_old_revisions,
-        partial(purge_old_versions, retain_how_many=retain_how_many),
-    ]
-    for purge_function in purge_functions:
-        packages_to_delete.update(purge_function(packages))
-        packages = [p for p in packages if p not in packages_to_delete]
-    return packages_to_delete
-
-
-def delete_packages(client: httpx.Client, packages: Set[Package], repo: str) -> None:
-    if not packages:
-        return
-    packages_str = [str(p) for p in sorted(packages, key=cmp_to_key(sort_cmp))]
-    print(f"The following packages are going to be removed from {repo}: {packages_str}")
-    data = {"PackageRefs": packages_str}
-    response = client.request("DELETE", f"/repos/{repo}/packages", json=data)
-    response.raise_for_status()
-
-
-def purge(client: httpx.Client, repo: str, names: Set[str], retain_how_many: int) -> None:
-    all_packages = list_packages(client, repo)
-    packages_to_delete: Set[Package] = set()
-    for name in names:
-        for arch in ["amd64", "arm64", "armhf"]:
-            packages = filter(lambda x: x.name == name and x.arch == arch, all_packages)
-            packages_to_delete.update(purge_old_packages(list(packages), retain_how_many))
-    delete_packages(client, packages_to_delete, repo)
-
-
-@contextmanager
-def upload_directory(client: httpx.Client) -> Iterator[str]:
-    directory = str(uuid.uuid4())
-    yield directory
-    response = client.delete(f"/files/{directory}")
-    if response.status_code != 200:
-        print(f"Failed to remove upload directory: {directory}")
-
-
-def upload_packages(client: httpx.Client, packages: List[Path], repo: str) -> None:
-    with upload_directory(client) as directory:
-        files = {file.name: file.open("rb") for file in packages}
-        print(f"Uploading {len(packages)} packages to directory {directory}")
-        response = client.post(f"/files/{directory}", files=files)
-        response.raise_for_status()
-        response = client.post(f"/repos/{repo}/file/{directory}?noRemove=1")
-        response.raise_for_status()
-
-
-def push(
-    repo: str,
-    package_directory: Path,
-    retain: int,
-    server_url: str,
-    ca_cert: str,
-    client_cert: str,
-    client_key: str,
-) -> None:
-    if package_directory.is_dir() is False:
-        print(f"{package_directory} is not a directory")
-        sys.exit(1)
-
-    packages = list(package_directory.glob("*.deb"))
-    print(f"Found {len(packages)} packages in {package_directory}")
-
-    if not packages:
-        return
-
-    with client_factory(server_url, ca_cert, client_cert, client_key) as client:
-        response = client.get("/repos")
-        response.raise_for_status()
-        repos = [r["Name"] for r in response.json()]
-        if repo not in repos:
-            print(f"Aptly repository {repo} not found.")
+    @classmethod
+    def raise_for_status(
+        cls, response: httpx.Response, custom_messages: dict[int, str] | None = None
+    ) -> None:
+        if response.is_success:
             return
-        upload_packages(client, packages, repo)
-        names = {file.name.split("_")[0] for file in packages}
-        for repo in repos:
-            purge(client, repo, names, retain)
 
+        if (
+            custom_messages
+            and (custom_message := custom_messages.get(response.status_code)) is not None
+        ):
+            message = custom_message
+        elif response.has_redirect_location:
+            message = (
+                f"{response.status_code} {response.reason_phrase} for url "
+                f"{response.url}\n"
+                f"Redirect location: '{response.headers['location']}'\n"
+            )
+        else:
+            message = (
+                f"{response.status_code} {response.reason_phrase} for url {response.url}"
+            )
+        raise AptlyClientError(message, response)
 
-def publish(
-    repo: str,
-    server_url: str,
-    ca_cert: str,
-    client_cert: str,
-    client_key: str,
-) -> None:
-    with client_factory(server_url, ca_cert, client_cert, client_key) as client:
-        response = client.put(
-            f"/publish/{repo}",
-            json={
-                "ForceOverwrite": True,
-                "Signing": {"GpgKey": "wakemebot@protonmail.com", "Batch": True},
-            },
+    @contextlib.contextmanager
+    def files_upload(self, package_paths: Iterable[Path]) -> Generator[str, None, None]:
+        upload_directory = str(uuid4())
+        try:
+            file_descriptors = {path.name: path.open("rb") for path in package_paths}
+        except OSError as e:
+            raise AptlyClientError(str(e))
+        response = self._client.post(f"/files/{upload_directory}", files=file_descriptors)
+        self.raise_for_status(response)
+        try:
+            yield upload_directory
+        finally:
+            try:
+                self._client.delete(f"/files/{upload_directory}")
+            except httpx.HTTPError as e:
+                raise AptlyClientError(str(e))
+
+    def repo_list(self) -> list[AptlyRepo]:
+        try:
+            response = self._client.get("/repos")
+        except httpx.HTTPError as e:
+            raise AptlyClientError(str(e))
+        self.raise_for_status(response)
+        return [AptlyRepo.parse_obj(repo) for repo in response.json()]
+
+    def repo_list_packages(self, repo_name: str) -> list[AptlyPackageRef]:
+        try:
+            response = self._client.get(f"/repos/{repo_name}/packages")
+        except httpx.HTTPError as e:
+            raise AptlyClientError(str(e))
+        self.raise_for_status(
+            response, {404: _REPO_DOES_NOT_EXIST_ERROR.format(repo_name)}
         )
-        response.raise_for_status()
-        print(response.json())
+        binary_packages = [p[1:] for p in response.json() if p[0] == "P"]
+        return [AptlyPackageRef(*p.split(" ")) for p in binary_packages]
+
+    def repo_remove_packages(
+        self, repo_name: str, packages: Iterable[AptlyPackageRef]
+    ) -> None:
+        json = {"PackageRefs": [package.ref for package in packages]}
+        try:
+            response = self._client.request(
+                "DELETE", f"/repos/{repo_name}/packages", json=json
+            )
+        except httpx.HTTPError as e:
+            raise AptlyClientError(str(e))
+        self.raise_for_status(response)
+
+    def repo_add_packages(self, repo_name: str, upload_directory: str) -> None:
+        response = self._client.post(
+            f"/repos/{repo_name}/file/{upload_directory}?noRemove=1"
+        )
+        self.raise_for_status(
+            response, {404: _REPO_DOES_NOT_EXIST_ERROR.format(repo_name)}
+        )
+
+    def publish_update(self, prefix: str, gpg_key: str | None = None) -> None:
+        try:
+            json = (
+                {
+                    "ForceOverwrite": True,
+                    "Signing": {"GpgKey": gpg_key, "Batch": True},
+                },
+            )
+            response = self._client.put(f"/publish/{prefix}", json=json)
+        except httpx.HTTPError as e:
+            raise AptlyClientError(str(e))
+        self.raise_for_status(response)
--- tests/test_aptly.py
+++ tests/test_aptly.py
@@ -1,119 +1,260 @@
-from unittest.mock import patch
+import json
+from unittest.mock import Mock, patch
 
-from wakemebot.aptly import (
-    Package,
-    delete_packages,
-    parse_packages,
-    purge,
-    purge_old_patches,
-    purge_old_revisions,
-    purge_old_versions,
-)
+import httpx
+import pytest
+
+from wakemebot.aptly import AptlyClient, AptlyClientError, AptlyPackageRef
 
 
-def test_parse_packages_should_return_package_list():
-    data = [
-        "Parmhf kubectl 1.0.0-1 16bd211dbbf80dc6",
-        "Parmhf kubectl 1.1.0-1 16bd211dbbf80dc7",
-        "Skubectl 1.1.0~ops2deb 16bd211dbbf80dc6",
+@patch("wakemebot.aptly.uuid4", Mock(return_value="someuuid"))
+def test_files_upload_uploads_files_to_directory(httpx_mock, tmp_path):
+    # Given
+    package_path = tmp_path / "package.deb"
+    package_path.touch()
+    httpx_mock.add_response(
+        method="POST",
+        url="http://test_url/files/someuuid",
+    )
+    httpx_mock.add_response(
+        method="DELETE",
+        url="http://test_url/files/someuuid",
+    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    with client.files_upload([package_path]) as upload_directory:
+        pass
+
+    # Then
+    first_request = httpx_mock.get_requests()[0]
+    assert "multipart/form-data;" in first_request.headers["content-type"]
+    assert upload_directory == "someuuid"
+
+
+@patch("wakemebot.aptly.uuid4", Mock(return_value="someuuid"))
+def test_files_upload_deletes_upload_directory_when_error_occurs_in_with(
+    httpx_mock, tmp_path
+):
+    # Given
+    package_path = tmp_path / "package.deb"
+    package_path.touch()
+    httpx_mock.add_response(
+        method="POST",
+        url="http://test_url/files/someuuid",
+    )
+    httpx_mock.add_response(
+        method="DELETE",
+        url="http://test_url/files/someuuid",
+    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    with pytest.raises(OSError) as exc:
+        with client.files_upload([package_path]):
+            raise OSError("great error")
+
+    # Then
+    exc.match("great error")
+
+
+def test_files_upload__raises_when_a_file_does_not_exist(httpx_mock, tmp_path):
+    # Given
+    client = AptlyClient("http://test_url")
+
+    # When
+    with pytest.raises(AptlyClientError) as exc:
+        with client.files_upload([tmp_path / "notfound"]):
+            pass
+
+    # Then
+    assert exc.match(r"No such file or directory: .+notfound")
+
+
+def test_files_upload__raises_when_a_file_is_a_directory(httpx_mock, tmp_path):
+    # Given
+    client = AptlyClient("http://test_url")
+
+    # When
+    with pytest.raises(AptlyClientError) as exc:
+        with client.files_upload([tmp_path]):
+            pass
+
+    # Then
+    assert exc.match(r"Is a directory: .+")
+
+
+def test_repo_list__returns_parsed_list_of_aptly_repositories(httpx_mock):
+    # Given
+    httpx_mock.add_response(
+        url="http://test_url/repos",
+        json=[
+            {
+                "Name": "myrepo",
+                "Comment": "comment",
+                "DefaultDistribution": "buster",
+                "DefaultComponent": "main",
+            }
+        ],
+    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    repos = client.repo_list()
+
+    # Then
+    assert repos[0].name == "myrepo"
+    assert repos[0].comment == "comment"
+    assert repos[0].default_distribution == "buster"
+    assert repos[0].default_component == "main"
+
+
+def test_repo_list__raises_on_httpx_errors(httpx_mock):
+    # Given
+    httpx_mock.add_exception(httpx.ReadTimeout("Unable to read within timeout"))
+
+    # When
+    client = AptlyClient("http://test_url")
+
+    with pytest.raises(AptlyClientError) as exc:
+        client.repo_list()
+
+    # Then
+    exc.match("Unable to read within timeout")
+
+
+def test_repo_list_packages__returns_package_list_when_http_request_succeeds(httpx_mock):
+    # Given
+    httpx_mock.add_response(
+        url="http://test_url/repos/myrepo/packages",
+        json=[
+            "Parmhf kubectl 1.0.0-1 16bd211dbbf80dc6",
+            "Parmhf kubectl 1.1.0-1 16bd211dbbf80dc7",
+            "Skubectl 1.1.0~ops2deb 16bd211dbbf80dc6",
+        ],
+    )
+
+    expected_package_list = [
+        AptlyPackageRef(
+            arch="armhf", name="kubectl", version="1.0.0-1", uid="16bd211dbbf80dc6"
+        ),
+        AptlyPackageRef(
+            arch="armhf", name="kubectl", version="1.1.0-1", uid="16bd211dbbf80dc7"
+        ),
     ]
-    assert parse_packages(data) == [
-        Package(arch="armhf", name="kubectl", version="1.0.0-1", uid="16bd211dbbf80dc6"),
-        Package(arch="armhf", name="kubectl", version="1.1.0-1", uid="16bd211dbbf80dc7"),
-    ]
+
+    # When
+    client = AptlyClient("http://test_url")
+    packages = client.repo_list_packages("myrepo")
+
+    # Then
+    assert packages == expected_package_list
 
 
-def test_purge_old_revisions__should_only_keep_latest_revision_of_a_package():
+def test_repo_list_packages__raises_aptly_client_error_on_client_error(httpx_mock):
+    # Given
+    httpx_mock.add_response(
+        url="http://test_url/repos/myrepo/packages",
+        status_code=404,
+    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    with pytest.raises(AptlyClientError) as exc:
+        client.repo_list_packages("myrepo")
+
+    # Then
+    assert exc.match("Repo 'myrepo' does not exist")
+
+
+def test_repo_list_packages__raises_aptly_client_error_on_server_error(httpx_mock):
+    # Given
+    httpx_mock.add_response(
+        url="http://test_url/repos/myrepo/packages",
+        status_code=500,
+    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    with pytest.raises(AptlyClientError) as exc:
+        client.repo_list_packages("myrepo")
+
+    # Then
+    assert exc.match(
+        "500 Internal Server Error for url http://test_url/repos/myrepo/packages"
+    )
+
+
+def test_repo_add_packages__adds_packages_from_upload_directory_to_repo(
+    httpx_mock, tmp_path
+):
+    # Given
+    httpx_mock.add_response(
+        method="POST",
+        url="http://test_url/repos/myrepo/file/someuuid?noRemove=1",
+    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    client.repo_add_packages("myrepo", "someuuid")
+
+    # Then
+
+
+def test_repo_add_packages__raises_when_repo_does_not_exist(httpx_mock, tmp_path):
+    # Given
+    httpx_mock.add_response(status_code=404)
+    client = AptlyClient("http://test_url")
+
+    # When
+    with pytest.raises(AptlyClientError) as exc:
+        client.repo_add_packages("myrepo", "someuuid")
+
+    # Then
+    assert exc.match("Repo 'myrepo' does not exist")
+
+
+def test_repo_remove_packages__does_delete_request_with_package_list_in_body(httpx_mock):
+    # Given
     packages = [
-        Package("_", "_", "2.0.1-2~ops2deb", "_"),
-        Package("_", "_", "2.0.1-3~ops2deb", "_"),
-        Package("_", "_", "2.0.3-1~ops2deb", "_"),
-        Package("_", "_", "2.0.3-2~ops2deb", "_"),
-        Package("_", "_", "2.0.3-4~ops2deb", "_"),
-    ]
-    packages = purge_old_revisions(packages)
-    assert packages == [
-        Package("_", "_", "2.0.1-2~ops2deb", "_"),
-        Package("_", "_", "2.0.3-1~ops2deb", "_"),
-        Package("_", "_", "2.0.3-2~ops2deb", "_"),
+        AptlyPackageRef("amd64", "kubectl", "1.0.0-2~ops2deb", "_"),
+        AptlyPackageRef("amd64", "kubectl", "1.0.1-3~ops2deb", "_"),
     ]
 
+    httpx_mock.add_response(
+        method="DELETE",
+        url="http://test_url/repos/myrepo/packages",
+    )
 
-def test_purge_old_patches__should_only_keep_latest_patch_version_of_a_package():
-    packages = [
-        Package("_", "_", "notsemver-1~ops2deb", "_"),
-        Package("_", "_", "2.0.1-1~ops2deb", "_"),
-        Package("_", "_", "2.0.2-1~ops2deb", "_"),
-        Package("_", "_", "2.0.3-1~ops2deb", "_"),
-        Package("_", "_", "2.1.2-1~ops2deb", "_"),
-        Package("_", "_", "2.1.3-1~ops2deb", "_"),
-    ]
-    packages = purge_old_patches(packages)
-    assert packages == [
-        Package("_", "_", "2.0.1-1~ops2deb", "_"),
-        Package("_", "_", "2.0.2-1~ops2deb", "_"),
-        Package("_", "_", "2.1.2-1~ops2deb", "_"),
-    ]
-
-
-def test_purge_old_versions__should_only_keep_latest_versions_of_a_package():
-    packages = [
-        Package("_", "_", "2.0.1-2~ops2deb", "_"),
-        Package("_", "_", "2.0.1-3~ops2deb", "_"),
-        Package("_", "_", "2.0.3-2~ops2deb", "_"),
-    ]
-    packages = purge_old_versions(packages, 2)
-    assert packages == [
-        Package("_", "_", "2.0.1-2~ops2deb", "_"),
-    ]
-
-
-@patch("wakemebot.aptly.httpx.Client", autospec=True)
-def test_delete_packages__should_send_properly_formatted_request(mock_client):
-    packages = {
-        Package("amd64", "kubectl", "1.0.1-3~ops2deb", "_"),
-        Package("amd64", "kubectl", "1.0.0-2~ops2deb", "_"),
+    expected_request_dict = {
+        "PackageRefs": [
+            "Pamd64 kubectl 1.0.0-2~ops2deb _",
+            "Pamd64 kubectl 1.0.1-3~ops2deb _",
+        ]
     }
-    delete_packages(mock_client, packages, "devops")
-    mock_client.request.assert_called_once_with(
-        "DELETE",
-        "/repos/devops/packages",
-        json={
-            "PackageRefs": [
-                "Pamd64 kubectl 1.0.0-2~ops2deb _",
-                "Pamd64 kubectl 1.0.1-3~ops2deb _",
-            ]
-        },
-    )
+
+    # When
+    client = AptlyClient("http://test_url")
+    client.repo_remove_packages("myrepo", packages)
+
+    # Then
+    assert json.loads(httpx_mock.get_request().content) == expected_request_dict
 
 
-@patch(
-    "wakemebot.aptly.list_packages",
-    return_value=[
-        Package("amd64", "kubectl", "1.0.1-3~ops2deb", "_"),
-        Package("amd64", "kubectl", "1.0.1-2~ops2deb", "_"),
-        Package("amd64", "kubectl", "1.0.2-2~ops2deb", "_"),
-        Package("amd64", "kubectl", "1.0.2-1~ops2deb", "_"),
-        Package("amd64", "kustomize", "0.12.0-1~ops2deb", "_"),
-        Package("amd64", "kustomize", "0.13.0-1~ops2deb", "_"),
-        Package("amd64", "kustomize", "0.15.0-1~ops2deb", "_"),
-        Package("amd64", "kustomize", "0.14.0-1~ops2deb", "_"),
-        Package("armhf", "kustomize", "0.12.0-1~ops2deb", "_"),
-        Package("armhf", "kustomize", "0.12.0-2~ops2deb", "_"),
-    ],
-)
-@patch("wakemebot.aptly.httpx.Client", autospec=True)
-def test_purge__should_delete_old_package_versions_and_revisions(_, mock_client):
-    purge(mock_client, "devops", {"kubectl", "kustomize"}, retain_how_many=3)
-    mock_client.request.assert_called_with(
-        "DELETE",
-        "/repos/devops/packages",
-        json={
-            "PackageRefs": [
-                "Pamd64 kustomize 0.12.0-1~ops2deb _",
-                "Parmhf kustomize 0.12.0-1~ops2deb _",
-                "Pamd64 kubectl 1.0.1-2~ops2deb _",
-                "Pamd64 kubectl 1.0.2-1~ops2deb _",
-            ]
-        },
+def test_publish_update__does_put_request_with_prefix_on_publish_endpoint(httpx_mock):
+    # Given
+    httpx_mock.add_response(
+        method="PUT",
+        url="http://test_url/publish/myprefix:.",
     )
+
+    expected_request_dict = [
+        {"ForceOverwrite": True, "Signing": {"GpgKey": "mykey", "Batch": True}}
+    ]
+
+    # When
+    client = AptlyClient("http://test_url")
+    client.publish_update("myprefix:.", "mykey")
+
+    # Then
+    assert json.loads(httpx_mock.get_request().content) == expected_request_dict
