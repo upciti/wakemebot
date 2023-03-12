6960dcaef38b6e43fa61981954d4aa8410cc79b1
Simon <simon.guigui@parkoview.com>
2023-03-12 21:48:32 +0100

    refactor!: add/delete packages and publish prefix with sync command

--- src/wakemebot/sync.py
+++ src/wakemebot/sync.py
@@ -34,3 +34,12 @@ def remove_packages_from_repos(client: AptlyClient, delta: Ops2debDelta) -> None
                 ):
                     to_remove.append(repo_package)
         client.repo_remove_packages(repo.name, to_remove)
+
+
+def add_packages_to_repos(
+    client: AptlyClient, package_directory: Path, repos_prefix: str
+) -> None:
+    for package in package_directory.glob("**/*.deb"):
+        component = package.parent.name
+        with client.files_upload([package]) as upload_directory:
+            client.repo_add_packages(f"{repos_prefix}{component}", upload_directory)
